8356e174245e413db9aac903eee883a52da47a97
neighbor983 <kenneth.ray.baker@gmail.com>
2017-02-21 00:02:20 +0000

    express and start page is set up

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # MEAN-Angular2
+
+M - mongoDB - need to go to www.mlab.com to set up a database
+E - express - (4.14.1) 
+A - angular2 -
+N - nodejs - (7.5.0)
+
+Also using:
+body-parser 
+ejs
+mongojs
+
+started with below video:
+https://www.youtube.com/watch?v=PFP0oXNNveg
---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 8080;
+
+var index = require('./routes/index');
+
+var app = express();
+
+//View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html',require('ejs').renderFile);
+
+//Set Static folder
+app.use(express.static(path.join(__dirname, 'client')));
+
+//Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+//Routes
+app.use('/', index);
+app.listen(port, function(){
+   console.log('listening on port ' + port); 
+});
